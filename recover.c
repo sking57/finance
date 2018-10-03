616e562b852c3b86e87dc111e8c9777ebb63e52a
sking57 <sking57@users.noreply.github.com>
2018-10-03 21:56:38 +0000

    20181003T215553Z

---
+++ recover.c
@@ -0,0 +1,78 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    // ensure proper usage
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ./recover image\n");
+        return 1;
+    }
+    // remember filenames
+    char *rawfile = argv[1];
+
+    // declare that this file pointer exists
+    FILE *img = NULL;
+
+    // open image file
+    FILE *inptr = fopen(rawfile, "r");
+
+    // make sure image is opened
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", rawfile);
+        return 2;
+    }
+    // initialize counter, which will keep track of how many JPEGs we have found
+    int counter = 0;
+
+    // initialize this so we know later that we have not yet seen a jpeg (indicates we are on the first jpeg)
+    int injpeg = 0;
+
+    // assign jpeg as an array of characters (the lenghth used is arbitrary)
+    char jpeg[50];
+
+    //create temporary buffer that will be filled with 512 bytes each time through the loop
+    unsigned char buffer[512];
+
+    // repeat these steps until the entire card is read through
+    while (fread(&buffer, 512, 1, inptr) == 1)
+    {
+        // check to see if the beginning of each block resembles a jpeg
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            // if this is not the first jpeg we've seen, close current file before continuing
+            if (injpeg == 1)
+            {
+                fclose(img);
+            }
+            // create new jpeg file
+            sprintf(jpeg, "%03i.jpg", counter);
+
+            // open file (img is a FILE * declared earlier)
+            img = fopen(jpeg, "w");
+
+            // keeps track of file number
+            counter ++;
+
+            // once we pass this step, we are in a jpeg and need it to be known!
+            injpeg = 1;
+        }
+        // if we are in a jpeg, we have to recover 512 bytes (write them into the jpeg file). Otherwise, just go to next block
+        if (injpeg == 1)
+        {
+            fwrite(&buffer, 512, 1, img);
+        }
+    }
+    // close infile
+    fclose(inptr);
+
+    // close remaining file
+    fclose(img);
+
+    // success
+    return 0;
+}
